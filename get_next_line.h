fc6d6719d8dce2972a074f687ab8c06fbbdd5075
Maxime2 SIMON <maxisimo@e1r3p10.42.fr>
2018-10-10 11:01:11 +0200

    clean

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxisimo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/17 12:59:45 by maxisimo          #+#    #+#             */
+/*   Updated: 2018/02/08 10:12:51 by maxisimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+# define BUFF_SIZE 42
+
+int			get_next_line(const int fd, char **line);
+
+#endif
